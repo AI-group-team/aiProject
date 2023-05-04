94854cc537f1cae2997d5b0a4ec7c9c5c12cb903
biny-am <binyamseleshes@gmail.com>
2023-05-04 00:16:18 -0700

    My Individual Assignment Binyam Seleshe Rock paper S . . .

---
+++ Binyam_Seleshe/Readme.txt
@@ -0,0 +1,30 @@
+Rock, Paper, Scissors Game
+
+This is a simple rock, paper, scissors game implemented in Python. The game includes a basic AI algorithm that randomly selects a move for the computer player.
+
+
+Requirements
+
+This game requires Python 3 and the Tkinter library.
+
+
+Usage
+
+To play the game, run the game.py file in a Python interpreter:
+The game will open in a window. Click on the "Rock", "Paper", or "Scissors" button to select your move. The game will then execute the AI algorithm and determine the winner. 
+The result will be displayed in a label. Click on the "Play again" button to reset the game and play again.
+
+Implementation
+
+The game is implemented using a combination of basic Python functions and the Tkinter library for the user interface. The play_game function determines the winner based on the
+traditional rules of rock, paper, scissors.The ai_move function uses the random library to select a move for the computer player.
+The user interface is implemented using the Tkinter library. The GameGUI class creates the window and widgets, and the play_game and reset methods handle the game logic and GUI updates.
+
+Future Improvements
+
+This game is a very basic implementation of an AI algorithm. There are many more advanced algorithms that could be used to make the AI player more challenging. Additionally, the GUI could
+be improved with more advanced widgets and styling.
+
+Credits
+
+This game was created by Binyam Seleshe. Feel free to use and modify the code for your own purposes.
---
+++ Binyam_Seleshe/game.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+import random
+
+# Define possible moves
+moves = ['rock', 'paper', 'scissors']
+
+# Define AI function
+def ai_move():
+    return random.choice(moves)
+
+# Define game logic
+def play_game(player_move):
+    ai_choice = ai_move()
+    
+    # Determine winner
+    if player_move == ai_choice:
+        return 'tie'
+    elif player_move == 'rock' and ai_choice == 'scissors':
+        return 'player'
+    elif player_move == 'paper' and ai_choice == 'rock':
+        return 'player'
+    elif player_move == 'scissors' and ai_choice == 'paper':
+        return 'player'
+    else:
+        return 'ai'
+
+# Define GUI
+class GameGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Rock, Paper, Scissors")
+        self.root.geometry("300x200")
+
+        self.label = tk.Label(self.root, text="welcome to the game:")
+        self.label.pack(pady=10)
+        
+        self.label = tk.Label(self.root, text="Choose your move:")
+        self.label.pack(pady=10)
+        
+        self.rock_button = tk.Button(self.root, text="Rock", command=lambda: self.play_game('rock'))
+        self.rock_button.pack(pady=5)
+        
+        self.paper_button = tk.Button(self.root, text="Paper", command=lambda: self.play_game('paper'))
+        self.paper_button.pack(pady=5)
+        
+        self.scissors_button = tk.Button(self.root, text="Scissors", command=lambda: self.play_game('scissors'))
+        self.scissors_button.pack(pady=5)
+        
+        self.result_label = tk.Label(self.root, text="")
+        self.result_label.pack(pady=10)
+        
+        self.play_again_button = tk.Button(self.root, text="Play again", command=self.reset)
+        
+        self.root.mainloop()
+    
+    def play_game(self, player_move):
+        winner = play_game(player_move)
+        
+        # Display result
+        if winner == 'tie':
+            self.result_label.config(text="It's a tie!")
+        else:
+            self.result_label.config(text=f"{winner.capitalize()} wins!")
+        
+        # Remove buttons and show play again button
+        self.rock_button.pack_forget()
+        self.paper_button.pack_forget()
+        self.scissors_button.pack_forget()
+        self.play_again_button.pack(pady=10)
+    
+    def reset(self):
+        # Reset GUI
+        self.result_label.config(text="")
+        self.play_again_button.pack_forget()
+        self.rock_button.pack(pady=5)
+        self.paper_button.pack(pady=5)
+        self.scissors_button.pack(pady=5)
+
+# Create GUI instance
+game_gui = GameGUI()
